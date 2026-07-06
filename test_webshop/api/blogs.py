16d5086aa9a2e2ed9f8546b12d59c4cc7b89cb88
poojadharni <poojarajuhe@gmail.com>
2026-07-06 11:53:22 +0000

    updated code

--- test_webshop/api/blogs.py
+++ test_webshop/api/blogs.py
@@ -1,16 +1,14 @@
+# import frappe
 import frappe
 
 @frappe.whitelist(allow_guest=True)
 def get_blogs():
-
     blogs = frappe.get_all(
         "Blogs",
         fields=[
             "name",
-            "creation",
-            "blog_date",
             "header_image",
-            "thumbnail_image",
+            "thumbnail_image", 
             "description_heading_1",
             "description_1",
             "description_heading_2",
@@ -19,6 +17,7 @@ def get_blogs():
             "description_3",
             "description_heading_4",
             "description_4",
+            "blog_date",
             "sub_description_heading_1",
             "sub_description_heading_2",
             "sub_description_heading_3",
@@ -31,68 +30,69 @@ def get_blogs():
         order_by="creation desc"
     )
 
+    # Add full URL for images
     for blog in blogs:
         if blog.get("header_image"):
             blog["header_image"] = frappe.utils.get_url(blog["header_image"])
-
         if blog.get("thumbnail_image"):
             blog["thumbnail_image"] = frappe.utils.get_url(blog["thumbnail_image"])
 
     return blogs
 
 
+
 @frappe.whitelist(allow_guest=True)
 def get_blog(blog_name=None, url=None):
+    """
+    Fetch a single Blog by DocName or URL
+    """
 
-    blog = None
+    try:
+        blog = None
 
-    if url:
+        if url:
+            blog_list = frappe.get_all(
+                "Blogs",
+                filters={"url": url},
+                fields=["name"],
+                limit=1
+            )
 
-        blogs = frappe.get_all(
-            "Blogs",
-            filters={"url": url},
-            fields=["name"],
-            limit=1
-        )
+            if not blog_list:
+                return {}
 
-        if not blogs:
+            blog = frappe.get_doc("Blogs", blog_list[0].name)
+
+        # ✅ Fallback: DocName
+        elif blog_name:
+            blog = frappe.get_doc("Blogs", blog_name)
+
+        else:
             return {}
 
-        blog = frappe.get_doc("Blogs", blogs[0].name)
+        # ✅ Convert image fields to full URLs
+        if blog.header_image:
+            blog.header_image = frappe.utils.get_url(blog.header_image)
+        if blog.thumbnail_image:
+            blog.thumbnail_image = frappe.utils.get_url(blog.thumbnail_image)
 
-    elif blog_name:
+        return {
+            "name": blog.name,
+            "header_image": blog.header_image,
+            "thumbnail_image": blog.thumbnail_image,
+            "description_heading_1": blog.description_heading_1,
+            "description_1": blog.description_1,
+            "description_heading_2": blog.description_heading_2,
+            "description_2": blog.description_2,
+            "description_heading_3": blog.description_heading_3,
+            "description_3": blog.description_3,
+            "description_heading_4": blog.description_heading_4,
+            "description_4": blog.description_4,
+            "custom_html": blog.custom_html,
+            "url": blog.url,
+            "meta_title": blog.meta_title,
+            "meta_description": blog.meta_description
+        }
 
-        blog = frappe.get_doc("Blogs", blog_name)
-
-    else:
+    except frappe.DoesNotExistError:
         return {}
-
-    if blog.header_image:
-        blog.header_image = frappe.utils.get_url(blog.header_image)
-
-    if blog.thumbnail_image:
-        blog.thumbnail_image = frappe.utils.get_url(blog.thumbnail_image)
-
-    return {
-        "name": blog.name,
-        "creation": blog.creation,
-        "blog_date": blog.blog_date,
-        "header_image": blog.header_image,
-        "thumbnail_image": blog.thumbnail_image,
-        "description_heading_1": blog.description_heading_1,
-        "description_1": blog.description_1,
-        "description_heading_2": blog.description_heading_2,
-        "description_2": blog.description_2,
-        "description_heading_3": blog.description_heading_3,
-        "description_3": blog.description_3,
-        "description_heading_4": blog.description_heading_4,
-        "description_4": blog.description_4,
-        "sub_description_heading_1": blog.sub_description_heading_1,
-        "sub_description_heading_2": blog.sub_description_heading_2,
-        "sub_description_heading_3": blog.sub_description_heading_3,
-        "sub_description_heading_4": blog.sub_description_heading_4,
-        "custom_html": blog.custom_html,
-        "url": blog.url,
-        "meta_title": blog.meta_title,
-        "meta_description": blog.meta_description
-    }
